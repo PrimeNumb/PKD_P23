ffbe658939b5d226d336dfeea98912462e9e15dc
Skola/jobb <skolajobb@student-213-249.eduroam.uu.se>
2019-02-22 16:49:04 +0100

    added enemies module

---
+++ enemies.hs
@@ -0,0 +1,14 @@
+module Enemies where
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import System.Random
+import Debug.Trace
+import Main(makeRectangle)
+
+enemyObj1 :: Object
+enemyObj1 = Object { position = (0, 0),
+                     direction = (0, 0),
+                     speed = 0,
+                     boundingBox = ((0, 0), (0, 0)),
+                     graphic = rectangleSolid 70.0 20.0
+                   }
