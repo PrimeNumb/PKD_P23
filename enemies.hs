533cf5dc587a8be2da73fc230fc5179cad9b1a3d
Skola/jobb <skolajobb@student-213-249.eduroam.uu.se>
2019-02-24 15:06:12 +0100

    enemy funktioner

--- enemies.hs
+++ enemies.hs
@@ -11,7 +11,7 @@ enemyColor = blue
 
 enemyObj1 :: Object
 enemyObj1 = Object { position = (400, 250),
-                     direction = (-1.0, 0),
+                     direction = (0, 0),
                      speed = 100,
                      boundingBox = (0,0),
                      graphic = color enemyColor $ rectangleSolid (70.0) (20.0)
@@ -32,10 +32,18 @@ moveEnemy gs@(GameState {enemy=eny}) (ex, ey) = gs {enemy = newEnemy}
     (nx, ny) = (x+ex, y+ey)
     newEnemy = eny {position = (nx, ny)} -}
 
-                   
+
+changeDir:: Game -> (Float, Float) -> (Float, Float)
+changeDir gameState@(GameState {enemy=eny@(Object {position = pos, direction = dir})})
+   | fst(pos eny) <= 400 = (-1.0,0)
+  {-| fst(position enemy) => 200 = (1.0,0)-}
+   | snd(pos eny) <= 250 = (0,-1.0)
+  {-| snd(position enemy) => -100 = (0,1.0)-}
+           
 updateEnemy dt gameState@(GameState {enemy=enemy}) = newEnemy
   where
-    (dx,dy) = direction enemy
+    (dx,dy) = changeDir (position enemy) (direction enemy)
     enemySpeed = speed enemy
     v = (dx*enemySpeed*dt,dy*enemySpeed*dt)
     newEnemy = moveObject enemy v
+    
