47b8bb5ed9bc5d77175748ff4bc52f8cda52ef43
Nils Hermansson <hermansson.nils@gmail.com>
2019-02-24 11:01:14 +0100

    wdw Please enter the commit message for your changes. Lines starting

--- enemies.hs
+++ enemies.hs
@@ -14,6 +14,7 @@ enemyObj1 = Object { position = (400, 250),
                      speed = 100,
                      boundingBox = ((0, 0), (0, 0)),
                      graphic = color enemyColor $ rectangleSolid (70.0) (20.0)
+                   }
 
 --getEnemyPos :: Game -> (Float, Float)
 --getEnemyPos gameState = position $ enemy gameState
